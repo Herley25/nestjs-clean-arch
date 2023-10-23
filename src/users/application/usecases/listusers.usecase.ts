dc5be6de368f9e3289190ccb01bf31eb53bac802
Herley Oliveira <herleyoliveira19@gmail.com>
2023-10-22 22:25:54 -0300

    feat: create type output

--- src/users/application/usecases/listusers.usecase.ts
+++ src/users/application/usecases/listusers.usecase.ts
@@ -3,12 +3,17 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
 import { SearchInput } from '@/shared/application/dtos/search-input';
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dtos/paginate-output';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
 
 // Caso de uso para exibir os dados de um usuário
 export namespace ListUsersUseCase {
   export type Input = SearchInput;
 
-  export type Output = void;
+  export type Output = PaginationOutput<UserOutput>;
 
   //Criação do método
   export class UseCase implements DefaultUseCase<Input, Output> {
@@ -18,7 +23,14 @@ export namespace ListUsersUseCase {
     async execute(input: Input): Promise<Output> {
       const params = new UserRepository.SearchParams(input);
       const searchResult = await this.userRepository.search(params);
-      return;
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: UserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => {
+        return UserOutputMapper.toOutput(item);
+      });
+      return PaginationOutputMapper.toOutput(items, searchResult);
     }
   }
 }
