4fa0438388be16b37f4079a67ca2ae803a179592
Herley Oliveira <herleyoliveira19@gmail.com>
2023-10-29 21:26:54 -0300

    refactor: envConfig module

--- src/users/domain/errors/validation-error.ts
+++ src/users/domain/errors/validation-error.ts
@@ -1,10 +1,10 @@
-import { FieldsErrors } from "@/shared/domain/validators/validator-fields.interface";
+import { FieldsErrors } from '@/shared/domain/validators/validator-fields.interface';
 
 export class ValidationError extends Error {}
 
 export class EntityValidationError extends Error {
   constructor(public error: FieldsErrors) {
-    super(message: "Entity Validation Error")
-    this.name = 'Entity Validation Error'
+    super('Entity Validation Error');
+    this.name = 'Entity Validation Error';
   }
 }
